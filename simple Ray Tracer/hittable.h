47cfec0c5037b02b14f9f1900ec65639c8f4f655
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2024-09-04 15:39:33 +0100

    first commit

---
+++ simple Ray Tracer/hittable.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#ifndef HITTABLE_H
+#define HITTABLE_H
+
+class material;
+
+
+class hit_record {
+	public:
+		point3 p;
+		vec3 normal;
+		shared_ptr<material> mat;
+		double t;
+		bool front_face;
+		// NOTE: the parameter `outward_normal` is assumed to have unit length.
+		void set_face_normal(const ray& r, const vec3& outward_normal) {
+			front_face = dot(r.direction(), outward_normal) < 0;
+			normal = front_face ? outward_normal : -outward_normal;
+		}
+		//virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
+};
+
+class hittable {
+public:
+	virtual ~hittable() = default;
+	virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
+};
+
+
+
+
+
+
+
+#endif
