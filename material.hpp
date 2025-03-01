23d530c5b3bd290f057f314c00361db0707b4510
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2025-03-01 21:12:29 +0000

    textures

--- material.hpp
+++ material.hpp
@@ -2,6 +2,7 @@
 #define MATERIAL_H
 
 #include "hittable.hpp"
+#include "texture.hpp"
 
 class material
 {
@@ -18,7 +19,8 @@ public:
 class lambertian : public material
 {
 public:
-    lambertian(const color &albedo) : albedo(albedo) {}
+    lambertian(const color &albedo) : tex(make_shared<solid_color>(albedo)) {}
+    lambertian(shared_ptr<texture> tex) : tex(tex) {}
 
     bool scatter(const ray &r_in, const hit_record &rec, color &attenuation, ray &scattered)
         const override
@@ -31,12 +33,13 @@ public:
         }
 
         scattered = ray(rec.p, scatter_direction, r_in.time());
-        attenuation = albedo;
+        attenuation = tex->value(rec.u, rec.v, rec.p);
         return true;
     }
 
 private:
     color albedo;
+    shared_ptr<texture> tex;
 };
 
 class metal : public material
